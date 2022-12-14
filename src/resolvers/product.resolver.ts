b68fdc5cad09faabe861364309f37b1f76d3abde
neezar <rforlyffe@gmail.com>
2022-12-14 10:33:16 +0800

    feat: add product resolver

---
+++ src/resolvers/product.resolver.ts
@@ -0,0 +1,90 @@
+import { MinLength } from "class-validator";
+import {
+  Arg,
+  Field,
+  InputType,
+  Int,
+  Mutation,
+  Query,
+  registerEnumType,
+  Resolver,
+} from "type-graphql";
+import { Product } from "../entities/Product.entities";
+
+@InputType()
+export class ProductInput {
+  @Field()
+  @MinLength(2)
+  title: string;
+  @Field()
+  @MinLength(2)
+  description: string;
+  @Field()
+  price: number;
+  @Field()
+  quantity: number;
+  @Field()
+  picture: string;
+}
+
+enum Sorting {
+  Asc = "ASC",
+  Desc = "DESC",
+}
+
+registerEnumType(Sorting, {
+  name: "Sorting",
+  description: "Sort by price",
+});
+
+@InputType({ description: "Filter by title description, and sorting by price" })
+class ProductSearchInput {
+  @Field({ nullable: true })
+  title: string;
+  @Field({ nullable: true })
+  description: string;
+  @Field(() => Sorting, { nullable: true })
+  sort: Sorting;
+}
+
+@Resolver(Product)
+export class ProductResolver {
+  @Query(() => [Product], { nullable: true })
+  async searchProduct(
+    @Arg("input", { nullable: true })
+    { description, title, sort }: ProductSearchInput
+  ): Promise<Product[] | undefined> {
+    const query = Product.createQueryBuilder("product");
+
+    if (sort === Sorting.Asc) {
+      query.orderBy("product.price", "ASC");
+    } else if (sort === Sorting.Desc) {
+      query.orderBy("product.price", "DESC");
+    } else {
+      query.orderBy("product.price", "ASC");
+    }
+
+    if (title) {
+      query.where("product.title ILIKE :title", { title: `%${title}%` });
+    }
+    if (description) {
+      query.andWhere("product.description ILIKE :description", {
+        description: `%${description}%`,
+      });
+    }
+
+    return query.getMany();
+  }
+
+  @Query(() => Product, { nullable: true })
+  async getProductById(
+    @Arg("productId", () => Int) productId: number
+  ): Promise<Product | undefined> {
+    return Product.findOne(productId);
+  }
+
+  @Mutation(() => Product)
+  async createProduct(@Arg("input") input: ProductInput): Promise<Product> {
+    return Product.create({ ...input }).save();
+  }
+}
